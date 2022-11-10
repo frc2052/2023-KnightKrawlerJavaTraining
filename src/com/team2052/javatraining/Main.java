c0ed0c79ba496048762cbd816bafd348ce9eb917
Nicole Vanyo <2026vanyn57@moundsviewschools.org>
2022-11-09 19:46:28 -0600

    Started Lesspm 3 Assimgment 3 Code

--- src/com/team2052/javatraining/Main.java
+++ src/com/team2052/javatraining/Main.java
@@ -74,9 +74,10 @@ public class Main {
                     } else {
                         System.out.println("Invalid direction. Only \"E\" and \"B\" are valid");
                     }
-                    break;
-                    boolean hasSword = true; {
+                    { boolean hasSword = true;
                         System.out.println("With this mighty sword you can face off the scary monsters in the MoNsTeR RoOm!");
+                    break;
+        
                     }
             }
         }     
@@ -86,5 +87,17 @@ public class Main {
         // This method is the singular starting point of our application
     public static void main(String[] args) throws Exception {
         new Main();
+        public enum Direction {
+            NORTH,
+            EAST,
+            SOUTH,
+            WEST
+        }
+            public class Player {
+                public Player (Scanner input, String name){
+                    this.input = input;
+                    this.name = name;
+                }
+            }
     }
 }
