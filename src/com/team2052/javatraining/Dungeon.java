6fe2b274131fd1d4879d104f19b59465de99eaa1
caleb.fynewever <2026fynec65@moundsviewschools.org>
2022-11-30 19:33:39 -0600

    added rooms and dungeon code

---
+++ src/com/team2052/javatraining/Dungeon.java
@@ -0,0 +1,70 @@
+public class Dungeon {
+    private final Rooms[]dungeon = {
+        new Rooms(
+            "Room Alligator", //room 0
+            3,
+            -1,
+            4,
+            2,
+            false,
+            false
+        ),
+        
+        new Rooms(
+            "Room Bear", //room 1
+            3,
+            0,
+            2,
+            -1,
+            true,
+            false    
+        ),
+
+        new Rooms(
+            "Room Crocodile", //room 2
+            1,
+            -1,
+            -1,
+            -1,
+            false,
+            true
+        ),
+
+        new Rooms(
+            "Room Eagle", //room 3
+            5,
+            4,
+            0,
+            1,
+            false,
+            false
+        ),
+
+        new Rooms(
+            "Room Lizard", //room 4
+            1,
+            0,
+            3,
+            5,
+            false,
+            false
+        ),
+
+        new Rooms(
+            "Room DEATH", //room 5
+            -1,
+            -1,
+            -1,
+            -1,
+            true,
+            false
+        )
+
+    };
+
+
+
+    public Rooms getRoom(int room){
+        return dungeon [room];
+    }
+}
