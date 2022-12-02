1d499c77de3064d0b1aabde9bd3740ed4e2e2260
caleb.fynewever <2026fynec65@moundsviewschools.org>
2022-12-01 19:43:56 -0600

    finished most of code for dungeon, still need to bug fix

--- src/com/team2052/javatraining/Main.java
+++ src/com/team2052/javatraining/Main.java
@@ -1,127 +1,60 @@
 import java.util.Scanner;
 
-enum Direction{
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-}
-
 public class Main {
-    // Variable to store our instanced class for receiving input
-    // NOTE: notice this variable has yet to be set equal to anything, this is done on line 13
+
     private Scanner input;
 
-    private String inputValue;
-
-    // Variable to store the users name once they provide it
     private String name;
 
-    //Current player position
-    int currentPosition = 0;
+    private final Dungeon dungeon;
 
-    private boolean isDead = false;
-    private boolean hasWon = false;
-    private boolean hasSword = false;
-
+    private final Player player;
     public Main() {
-        // ASSIGNMENT A:
+
+         //initialize stuff
+        dungeon = new Dungeon();
+        
+        player = new Player(input, name);
+
         System.out.println("Hello player!");
 
         // ASSIGNMENT B:
         // Here we "initialize" or set our variable (input) equal to a new Scanner instance that receives input from 'System.in'
         input = new Scanner(System.in);
 
-        // Asks the user to input their name through the console
         System.out.println("Please enter your name below:");
         
-        // Gets the next available user input and sets the name variable to that input
         name = input.nextLine();
 
-        // Prints a customized welcome message for the user
         System.out.println("Welcome, " + name + "!");
 
-        // ASSIGNMENT C:
-            while(!isDead && !hasWon){
-                switch (currentPosition) {
-                    case 0:
-                        System.out.println("You are at the entrance, you can only go north. Type \"N\" to go north.");
-                        inputValue = input.nextLine();
-                        if (inputValue.toLowerCase().trim().equals("n")) {
-                            currentPosition = 1;
-                        } 
-                            else {
-                             System.out.println("Invalid direction. Only \"N\" is valid");
-                        }
-                             break;
-                    case 1:
-                        System.out.println("You are standing at a hallway intersection that moves in all four directions; north, south, east and west, choose your path");
-                        inputValue = input.nextLine();
-                        if (inputValue.toLowerCase().trim().equals("e")) {
-                            currentPosition = 2;
-                        } else if (inputValue.toLowerCase().trim().equals("n")) {
-                            currentPosition = 3;
-                        } else if (inputValue.toLowerCase().trim().equals("w")) {
-                            currentPosition = 4;
-                        } else if (inputValue.toLowerCase().trim().equals("s")) {
-                            currentPosition = 0;
-                        } else {
-                            System.out.println("Invalid direction. Only \"N\", \"S\", \"E\", \"W\",  are valid");
-                        }
-                        break;
-                    case 2:
-                        System.out.println("You have found a large wooden door that is latched from the outside. Do you enter \"E\" or Go Back \"B\"");
-                        inputValue = input.nextLine();
-                        if (inputValue.toLowerCase().trim().equals("e")) {
-                            System.out.println("You unlock the door and slowly push the door open.");
-                            System.out.println("As you enter the room a small candle on the table catches your attention.");
-                            System.out.println("As you are mesmerized by the flame, you fail to notice the witch who casts a spell to turn you to stone.");
-                            System.out.println("You will forever adorn her room. You have died.");
-                            
-                            isDead = true;
-                        } else if (inputValue.toLowerCase().trim().equals("b")) {
-                            currentPosition = 1;
-                        } else {
-                            System.out.println("Invalid direction. Only \"E\" and \"B\" are valid");
-                        }
-                        break;
-                    case 3:
-                        System.out.println("A dark archway stands before you. Do you enter \"E\" or Go Back \"B\"");
-                        inputValue = input.nextLine();
-                        if (inputValue.toLowerCase().trim().equals("e")) {
-                            System.out.println("You slowly walk through and find a massive ogre sleeping");
-                            System.out.println("Do you fight \"E\" or Go Back \"B\"");
-                            inputValue = input.nextLine();
-                        }
-                        if ((inputValue.toLowerCase().trim().equals("e")) && (hasSword)){
-                             System.out.println("After a long fight, you defeat the ogre and teleport out of the dungeon. Congrats, you have won!");
-                             hasWon = true;
-                        }
-                                else if ((inputValue.toLowerCase().trim().equals("e")) && (!hasSword)){
-                                System.out.println("You lost the fight horribly without a weapon, game over.");
-                                isDead = true;
-                                }
-                                else if (inputValue.toLowerCase().trim().equals("b")) {
-                                currentPosition = 1;
-                                }
-                        else {
-                            System.out.println("Invalid direction. Only \"E\" and \"B\" are valid");
-                        }
-                        break;
-                    case 4:
-                        System.out.println("You have found a gleaming sword! Continue on with your dungoneering");
-                        hasSword = true;
-                        currentPosition = 1;
-                        break;
+        while (!player.checkIsDead() || !player.checkHasWon()){
+                if (player.hasMoved()){
+                    System.out.println(dungeon.getRoom(player.getCurrentRoom()).getDescription());
+                }
+                player.processUserInput();
 
-            }
-            }
+                int desiredRoom = (dungeon.getNextRoom(player.getDesiredDirection(),player.getCurrentRoom()));
 
+                if (desiredRoom != -1)
+                    if (dungeon.getRoom(desiredRoom).isWinningRoom()){
+                        System.out.println("Congrats, you have escaped the dungeon!");
+                        player.playerWins();
+                    }
+                    if (dungeon.getRoom(desiredRoom).isDeathRoom()){
+                        System.out.println("you suck");
+                        player.killPlayer();
+                    }
+                else    
+                    System.out.println("You smacked into a wall");
+
+                break;
+
+        }
     }
-
     // This method is the singular starting point of our application
     public static void main(String[] args) throws Exception {
         new Main();
-
     }
+
 }
